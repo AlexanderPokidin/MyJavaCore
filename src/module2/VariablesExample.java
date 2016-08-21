afb729be140132e84841c4f242e307f0cc3f8b72
AlexanderPokidin <a.pokidin@gmail.com>
2016-08-21 14:13:55 +0300

    second module classwork commit

---
+++ src/module2/VariablesExample.java
@@ -0,0 +1,33 @@
+package module2;
+
+
+public class VariablesExample {
+
+    public static void main(String[] args) {
+        /* &&
+        true - true = true
+        true - false
+        false -true
+        false - false
+        */
+
+        /* ||
+        true - true
+        false - true
+        true - false
+        false - false = false
+         */
+
+        boolean isActive = true;
+        for (int k = 5; k < 10; k++) {
+            System.out.println(k);
+        }
+
+        System.out.println("hello");
+
+        if (true) {
+            System.out.println("test");
+        }
+
+    }
+}
---
+++ src/module2/testpackage/TestClass.java
@@ -0,0 +1,18 @@
+package module2.testpackage;
+
+
+public class TestClass {
+
+
+
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a&&b));
+        System.out.println("a || b = " + (a||b));
+        System.out.println("!(a && b) = " + !(a&&b));
+
+
+    }
+}
