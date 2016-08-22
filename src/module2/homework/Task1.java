8e317cc228b0c4eb651d137e1a599d1cafebb0c2
AlexanderPokidin <a.pokidin@gmail.com>
2016-08-22 20:54:24 +0300

    second module begin homework commit

---
+++ src/module2/homework/Task1.java
@@ -0,0 +1,18 @@
+package module2.homework;
+
+
+public class Task1 {
+
+    int sum(int data[]) {
+
+    }
+    return
+
+
+    public static void main(String[] args) {
+        double data[] = {52, 73.2, 9.6, 18, -6, 85.7, -15.4, 67, -58, -6};
+        {
+            System.out.println(data[data.length - 1]);
+        }
+    }
+}
