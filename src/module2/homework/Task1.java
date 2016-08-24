aab95066bbc0266e4aa8f0ed119cd278b6831ff1
AlexanderPokidin <a.pokidin@gmail.com>
2016-08-24 20:46:49 +0300

    second module homework #1

--- src/module2/homework/Task1.java
+++ src/module2/homework/Task1.java
@@ -1,18 +1,162 @@
 package module2.homework;
 
-
 public class Task1 {
 
-    int sum(int data[]) {
+    static int array[] = {25, -7, 96, 8, 6, -87, 14, 27, -8, 2};
+    static double data[] = {52, 3.2, 9.6, 18, -6, 5.7, -15.4, 67, -58, -6};
+
+
+    static int sum(int[] array) {  //sum(int array[])
+        int a = 0;
+        for (int item : array) {
+            a += item;
+        }
+        return a;
+    }
+
+    static double sum(double[] data) { //sum(double[])
+        double a = 0;
+        for (double item : data) {
+            a += item;
+        }
+        return a;
+    }
+
+    static int multiplication(int[] array) {  //multiplication(int array[])
+        int b = 1;
+        for (int item : array) {
+            b *= item;
+        }
+        return b;
+    }
+
+    static double multiplication(double[] data) {  //multiplication (double[])
+        double b = 1;
+        for (double item : data) {
+            b *= item;
+        }
+        return b;
+    }
+
+    static int modulus(int[] array) {  //modulus(int array[])
+        int c = array[0];
+        int d = array[9];
+        int e = c % d;
+
+        return e;
+    }
+
+    static double modulus(double[] data) {  //modulus(double[])
+        double c = data[0];
+        double d = data[9];
+        double e = c % d;
+
+        return e;
 
     }
-    return
+
+    static int min(int[] array) {   //min(int array[])
+        int f = array.length-1;
+        for (int item : array) {
+            if (item < f) {
+                f = item;
+            }
+        }
+        return f;
+    }
+
+    static double min(double[] date) {  //min(double[])
+        double f = date.length-1;
+        for (double item : date) {
+            if (item < f) {
+                f = item;
+            }
+        }
+        return f;
+    }
+
+    static int max(int[] array) {  //max(int array[])
+        int g = array.length-1;
+        for (int item : array) {
+            if (item > g) {
+                g = item;
+            }
+        }
+        return g;
+    }
+
+    static double max(double[] data) {   //max(double[])
+        double h = data.length-1;
+        for (double item : data) {
+            if (item > h) {
+                h = item;
+            }
+        }
+        return h;
+    }
+
+    static int maxPositive(int[] array) {  //maxPositive(int array[])
+        int k = 0;
+        for (int item : array) {
+            if (item > k) {
+                k = item;
+            }
+        }
+        return k;
+    }
+
+    static double maxPositive(double[] data) {  //maxPositive(double array[])
+        double k = 0;
+        for (double item : data) {
+            if (item > k) {
+                k = item;
+            }
+        }
+        return k;
+    }
+
+    static int secondLargest(int[] array) {  //secondLargest(int array[])
+        int l = array.length-1;
+        for (int item : array) {
+            if (l < item && item < max(array)) {
+                l = item;
+            }
+        }
+        return l;
+    }
+
+    static double secondLargest(double[] data) {  // secondLargest(double[])
+        double l = data.length-1;
+        for (double item : data) {
+            if (l < item && item < max(data)) {
+                l = item;
+            }
+        }
+        return l;
+    }
 
 
     public static void main(String[] args) {
-        double data[] = {52, 73.2, 9.6, 18, -6, 85.7, -15.4, 67, -58, -6};
-        {
-            System.out.println(data[data.length - 1]);
-        }
+
+        System.out.println(sum(array));
+        System.out.println(sum(data));
+
+        System.out.println(multiplication(array));
+        System.out.println(multiplication(data));
+
+        System.out.println(modulus(array));
+        System.out.println(modulus(data));
+
+        System.out.println(min(array));
+        System.out.println(min(data));
+
+        System.out.println(max(array));
+        System.out.println(max(data));
+
+        System.out.println(maxPositive(array));
+        System.out.println(maxPositive(data));
+
+        System.out.println(secondLargest(array));
+        System.out.println(secondLargest(data));
     }
 }
