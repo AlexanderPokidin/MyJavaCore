818c271382fd60e62e2ccc95409717666edcd696
alexander <a.pokidin@gmail.com>
2016-08-30 13:25:36 +0300

    homework 3.1 100%

--- src/module3/homework/task1/Solution.java
+++ src/module3/homework/task1/Solution.java
@@ -4,15 +4,13 @@ package module3.homework.task1;
 public class Solution {
 
     public static void main(String[] args) {
-        Bird walk = new Bird();
-        Bird fly = new Bird();
-        Bird sing = new Bird();
-        Bird bird = new Bird();
 
-        bird.walk();
-        bird.fly();
-        bird.sing();
-        bird.bird();
+        Bird printBird = new Bird();
+
+        printBird.walk();
+        printBird.fly();
+        printBird.sing();
+        printBird.bird();
 
     }
 }
