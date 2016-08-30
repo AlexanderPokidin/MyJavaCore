0c87845b3bb8409e032a036766e47dd98c9a2caa
alexander <a.pokidin@gmail.com>
2016-08-30 12:54:07 +0300

    homework 3.1 75%

--- src/module3/homework/task1/Bird.java
+++ src/module3/homework/task1/Bird.java
@@ -1,9 +1,14 @@
 package module3.homework.task1;
 
 
-public class Bird {
+public class Bird extends Animal {
 
     void fly() {
         System.out.println("I am flying");
     }
+    void sing() { System.out.println("I am singing"); }
+    void bird() { System.out.println("I am Bird"); }
+
 }
+
+
--- src/module3/homework/task1/Solution.java
+++ src/module3/homework/task1/Solution.java
@@ -4,6 +4,15 @@ package module3.homework.task1;
 public class Solution {
 
     public static void main(String[] args) {
+        Bird walk = new Bird();
+        Bird fly = new Bird();
+        Bird sing = new Bird();
+        Bird bird = new Bird();
+
+        bird.walk();
+        bird.fly();
+        bird.sing();
+        bird.bird();
 
     }
 }
