bf0bb1187c189ed1dd15cfef13f6a321220a7588
AlexanderPokidin <a.pokidin@gmail.com>
2016-09-01 23:21:44 +0300

    third module task 3.2 update

--- src/module3/homework/task2/Adder.java
+++ src/module3/homework/task2/Adder.java
@@ -2,10 +2,11 @@ package module3.homework.task2;
 
 
 public class Adder extends Arithmetic {
-    boolean check(Integer a, Integer b) {
-        boolean result = false;
-        if (a >= b)
-            result = true;
+    public boolean check(Integer a, Integer b) {
+        boolean result = false;{
+            if (a >= b)
+                result = true;
+        }
 
         return result;
     }
--- src/module3/homework/task2/Arithmetic.java
+++ src/module3/homework/task2/Arithmetic.java
@@ -2,7 +2,7 @@ package module3.homework.task2;
 
 
 public class Arithmetic {
-    int add(Integer a, Integer b) {
+    public int add(Integer a, Integer b) {
         Integer sum = a + b;
 
         return sum;
