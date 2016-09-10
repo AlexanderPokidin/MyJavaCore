a6f178b2df5031fc59ae16a98be6f7a9e2749729
AlexanderPokidin <a.pokidin@gmail.com>
2016-09-10 17:14:04 +0300

    fourth module, homework done

--- src/module4/homework/BankSystem.java
+++ src/module4/homework/BankSystem.java
@@ -2,9 +2,9 @@ package module4.homework;
 
 public interface BankSystem {
 
-    void withdrawOfUser(User user, int summ);
-    void fundUser(User user, int summ);
-    void transferMoney(User fromUser, User toUser, int summ);
+    void withdrawOfUser(User user, int amount);
+    void fundUser(User user, int amount);
+    void transferMoney(User fromUser, User toUser, int amount);
     void paySalary(User user);
 
 
--- src/module4/homework/BankSystemImpl.java
+++ src/module4/homework/BankSystemImpl.java
@@ -3,28 +3,28 @@ package module4.homework;
 public class BankSystemImpl implements BankSystem{
 
     @Override
-    public void withdrawOfUser(User user, int summ) {
+    public void withdrawOfUser(User user, int amount) {
         Bank userBank = user.getBank();
-        if (userBank.getLimitOfWithdrawAll() >= summ + userBank.getCommission(summ)) {
-            double newBalance = user.getBalance() - summ - userBank.getCommission(summ);
+        if (userBank.getLimitOfWithdrawAll() >= amount + userBank.getCommission(amount)) {
+            double newBalance = user.getBalance() - amount - userBank.getCommission(amount);
             user.setBalance(newBalance);
         }
     }
 
     @Override
-    public void fundUser(User user, int summ) {
+    public void fundUser(User user, int amount) {
         Bank userBank = user.getBank();
-        if (userBank.getLimitOfFunding() >= summ) {
-            double newBalance = user.getBalance() + summ;
+        if (userBank.getLimitOfFunding() >= amount) {
+            double newBalance = user.getBalance() + amount;
             user.setBalance(newBalance);
         }
     }
 
     @Override
-    public void transferMoney(User fromUser, User toUser, int summ) {
-        double newBalanceFrom = fromUser.getBalance() - summ;
+    public void transferMoney(User fromUser, User toUser, int amount) {
+        double newBalanceFrom = fromUser.getBalance() - amount;
         fromUser.setBalance(newBalanceFrom);
-        double newBalanceTo = toUser.getBalance() + summ;
+        double newBalanceTo = toUser.getBalance() + amount;
         toUser.setBalance(newBalanceTo);
     }
 
