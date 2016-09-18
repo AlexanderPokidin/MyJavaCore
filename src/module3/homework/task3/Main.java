5784ea45be33e97afb0589f742b2b7478cb4721c
AlexanderPokidin <a.pokidin@gmail.com>
2016-09-18 09:53:54 +0300

    3.3 fix

--- src/module3/homework/task3/Main.java
+++ src/module3/homework/task3/Main.java
@@ -8,20 +8,23 @@ public class Main {
 
         Date date = new Date();
 
-        Course chemistry = new Course(date, "John", "John", 25,"Smoozy");
-        Course physics = new Course();
-        Course mathematics = new Course();
-        Course history = new Course();
-        Course geography = new Course();
+        Course chemistry = new Course(date, "John");
+        Course physics = new Course(date, "Garry");
+        Course mathematics = new Course(date, "Tommy");
+        Course history = new Course(date, "Bruno");
+        Course geography = new Course(date, "Maria");
 
+        Course courses[] = {chemistry, mathematics, physics, geography, history};
 
+        Student vasya = new Student("Vasya", "Popov", 2);
+        Student vova = new Student("Baranov", courses);
 
+        CollegeStudent inna = new CollegeStudent("Inna", "Kozlova", 3);
+        CollegeStudent nina = new CollegeStudent("Papina", courses);
+        CollegeStudent ira = new CollegeStudent("KPI", 54, 425875625);
 
-
-
-
-
-
-
+        SpecialStudent damir = new SpecialStudent("Damir", "Oboev", 1);
+        SpecialStudent vahtang = new SpecialStudent("KPI", 47, 52487569);
+        SpecialStudent marat = new SpecialStudent(504875);
     }
 }
