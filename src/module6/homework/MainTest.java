47959a9cd5dd85e28a7c474c3b16647b4f81c993
alexander <a.pokidin@gmail.com>
2016-09-23 17:20:18 +0300

    Homework 6.1

--- src/module6/homework/MainTest.java
+++ src/module6/homework/MainTest.java
@@ -2,22 +2,18 @@ package module6.homework;
 
 public class MainTest {
 
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 
     public static void main(String[] args) {
         int testArray[] = {5, 7, 53, -6, 34, -1, 2};
 
 
-
-//        System.out.println(ArraysUtils.min(testArray));
-//        System.out.println(ArraysUtils.maxPositive(testArray));
-//        System.out.println(ArraysUtils.multiplication(testArray));
-//        System.out.println(ArraysUtils.modulus(testArray));
-//        System.out.println(ArraysUtils.secondLargest(testArray));
-        System.out.println(ArraysUtils.findEvenElements(testArray));
+        System.out.println(ArraysUtils.sum(testArray));
+        System.out.println(ArraysUtils.min(testArray));
+        System.out.println(ArraysUtils.maxPositive(testArray));
+        System.out.println(ArraysUtils.multiplication(testArray));
+        System.out.println(ArraysUtils.modulus(testArray));
+        System.out.println(ArraysUtils.secondLargest(testArray));
+        System.out.println(ArraysUtils.findEvenElements(testArray).toString());
         System.out.println();
 
 
