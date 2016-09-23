e22414ad69af596e6553937ebb200d18206c94b1
alexander <a.pokidin@gmail.com>
2016-09-23 17:21:08 +0300

    HW 6.1

---
+++ src/module6/homework/UserUtils.java
@@ -0,0 +1,15 @@
+package module6.homework;
+
+public class UserUtils {
+/*
+    private User[] uniqueUsers(User[] users) {};
+
+    private User[] usersWithContitionalBalance(User[] users, int balance) {};
+
+    private static User[] paySalaryToUsers(User[] users) {};
+
+    private static long[] getUsersId(User[] users) {};
+
+    private User[] deleteEmptyUsers(User[] users) {};
+*/
+}
