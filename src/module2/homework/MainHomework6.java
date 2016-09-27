2ff1da0ae3361b984018256723594535796102b3
AlexanderPokidin <a.pokidin@gmail.com>
2016-09-27 21:40:48 +0300

    Homework 6.2

---
+++ src/module2/homework/MainHomework6.java
@@ -0,0 +1,20 @@
+package module2.homework;
+
+
+import module6.homework.ArraysUtils;
+
+public class MainHomework6 {
+    public static void main(String[] args) {
+        int array[] = {25, -7, 96, 8, 6, -87, 14, 27, -8, 2};
+
+        ArraysUtils.sum(array);
+        ArraysUtils.min(array);
+        ArraysUtils.max(array);
+        ArraysUtils.maxPositive(array);
+        ArraysUtils.multiplication(array);
+        ArraysUtils.modulus(array);
+        ArraysUtils.secondLargest(array);
+        ArraysUtils.reverse(array);
+        ArraysUtils.findEvenElements(array);
+    }
+}
