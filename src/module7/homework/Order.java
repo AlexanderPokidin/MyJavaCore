6f4afd9319311f25b52976a2c4adba9f5adf9a2d
AlexanderPokidin <a.pokidin@gmail.com>
2016-10-02 19:25:32 +0300

    Homework 7.1

---
+++ src/module7/homework/Order.java
@@ -0,0 +1,21 @@
+package module7.homework;
+
+
+
+public class Order {
+    private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+    private User user;
+
+    public Order(long id, int price, Currency currency, String itemName, String shopIdentificator, User user) {
+        this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.itemName = itemName;
+        this.shopIdentificator = shopIdentificator;
+        this.user = user;
+    }
+}
