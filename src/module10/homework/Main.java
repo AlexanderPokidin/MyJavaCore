df3044520041dd87023d1177f4af1ffbfb323890
AlexanderPokidin <a.pokidin@gmail.com>
2016-11-01 22:07:57 +0200

    Homework 10.3

--- src/module10/homework/Main.java
+++ src/module10/homework/Main.java
@@ -1,16 +1,43 @@
 package module10.homework;
 
+import sn.User;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
         String str = null;
 
+        //Task1
         try {
             str.toUpperCase();
-
         } catch (NullPointerException e){
             System.out.println("String is null");
         } finally {
             System.out.println("Method is done");
         }
+        System.out.println("----------");
+
+        //Task2
+        String message = "I`m string";
+
+        try {
+            if (message.length() < 5) {
+                System.out.println(message.toUpperCase());
+            } else {
+                throw new MyException(message);
+            }
+        } catch (MyException e) {
+            System.out.println("MyException is caught");
+        }
+        System.out.println("----------");
+
+        //Task3
+        User user = null;
+        try {
+            user.getAge();
+        } catch (NullPointerException e1) {
+            System.out.println("User is empty");
+        }
+
+
     }
 }
--- src/module10/homework/MyException.java
+++ src/module10/homework/MyException.java
@@ -2,8 +2,6 @@ package module10.homework;
 
 public class MyException extends Exception{
 
-    private String message = "My first Exception";
-
     public MyException(String message) {
         super(message);
     }
